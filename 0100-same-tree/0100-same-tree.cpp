58eb95fad3f19ad7ef1dde6c3e52858de79ff6ad
Aakanksha <aakanksha.rathi@icloud.com>
2025-06-12 13:42:50 +0530

    Time: 0 ms (100%), Space: 12.7 MB (77.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,26 +11,30 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* p,vector<TreeNode*> &traverse){
-        if(p==NULL){
-            traverse.push_back(nullptr);
-            return;
-        }
-        traverse.push_back(p);
-        solve(p->left,traverse);
-        solve(p->right,traverse);
-    }
+    // void solve(TreeNode* p,vector<TreeNode*> &traverse){
+    //     if(p==NULL){
+    //         traverse.push_back(nullptr);
+    //         return;
+    //     }
+    //     traverse.push_back(p);
+    //     solve(p->left,traverse);
+    //     solve(p->right,traverse);
+    // }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-       vector<TreeNode*> tree1;
-       vector<TreeNode*> tree2;
-       solve(p,tree1);
-       solve(q,tree2);
-       if(tree1.size()!=tree2.size())return false;
-       for(int i=0;i<tree1.size();i++){
-        if(tree1[i]==NULL && tree2[i]!=NULL)return false;
-        if(tree2[i]==NULL && tree1[i]!=NULL)return false;
-        if(tree1[i] && tree2[i]&& (tree1[i]->val!=tree2[i]->val))return false;
-       }
-       return true;
+    //    vector<TreeNode*> tree1;
+    //    vector<TreeNode*> tree2;
+    //    solve(p,tree1);
+    //    solve(q,tree2);
+    //    if(tree1.size()!=tree2.size())return false;
+    //    for(int i=0;i<tree1.size();i++){
+    //     if(tree1[i]==NULL && tree2[i]!=NULL)return false;
+    //     if(tree2[i]==NULL && tree1[i]!=NULL)return false;
+    //     if(tree1[i] && tree2[i]&& (tree1[i]->val!=tree2[i]->val))return false;
+    //    }
+    //    return true;
+        if(p==NULL && q==NULL)return true;
+        else if(p==NULL || q==NULL)return false;
+        if(p->val!=q->val)return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
