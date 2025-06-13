75c615615ba56666aac15611ed14ee7d3dcf22d1
Aakanksha <aakanksha.rathi@icloud.com>
2025-06-14 02:46:49 +0530

    Time: 0 ms (100%), Space: 17.5 MB (77.2%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,29 +11,46 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, queue<TreeNode*> &v){
-        if(root==NULL){
-            return;
-        }
-        v.push(root);
-        solve(root->left,v);
-        solve(root->right,v);
-    }
+    // void solve(TreeNode* root, queue<TreeNode*> &v){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     v.push(root);
+    //     solve(root->left,v);
+    //     solve(root->right,v);
+    // }
     void flatten(TreeNode* root) {
-        if(root==NULL)return;
-        queue<TreeNode*> v;
-        solve(root,v);
-        TreeNode* curr=v.front();
-        v.pop();
-        while(!v.empty()){
-            TreeNode* next=v.front();
-            v.pop();
-            curr->left=NULL;
-            curr->right=next;
-            curr=next;
+        // if(root==NULL)return;
+        // queue<TreeNode*> v;
+        // solve(root,v);
+        // TreeNode* curr=v.front();
+        // v.pop();
+        // while(!v.empty()){
+        //     TreeNode* next=v.front();
+        //     v.pop();
+        //     curr->left=NULL;
+        //     curr->right=next;
+        //     curr=next;
+        // }
+        // curr->left=NULL;
+        // curr->right=NULL;
+        // return;
+        
+
+        //imp approach
+        TreeNode* curr=root;
+        while(curr){
+            if(curr->left){
+            TreeNode* predecessor=curr->left;
+            while(predecessor->right){
+                predecessor=predecessor->right;
+            }
+            
+             predecessor->right=curr->right;
+             curr->right=curr->left;
+             curr->left=NULL;
         }
-        curr->left=NULL;
-        curr->right=NULL;
-        return;
+           curr=curr->right;
+        }
     }
 };
