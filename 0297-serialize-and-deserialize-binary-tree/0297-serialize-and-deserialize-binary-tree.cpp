56c1cf03f9dbbd9e464362f9618294a737a84362
Aakanksha <aakanksha.rathi@icloud.com>
2025-06-15 12:20:17 +0530

    Time: 35 ms (25.2%), Space: 45.8 MB (15.62%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(root==NULL)return "N";
+        string curr= to_string(root->val);
+        string l=serialize(root->left);
+        string r=serialize(root->right);
+        return curr+","+l+","+r;
+    }
+
+    // Decodes your encoded data to tree.
+    
+    TreeNode* solve(queue<string>& q){
+        if(q.empty())return NULL;
+        if(q.front()=="N"){
+            q.pop();
+            return NULL;
+        }
+        TreeNode* curr=new TreeNode(stoi(q.front()));
+        q.pop();
+        curr->left = solve(q);
+        curr->right=solve(q);
+        return curr;
+        
+}
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        string str;
+        queue<string> q;
+        while(getline(ss,str,',')){
+            q.push(str);
+        }
+        if(q.empty())return NULL;
+        return solve(q);
+
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
