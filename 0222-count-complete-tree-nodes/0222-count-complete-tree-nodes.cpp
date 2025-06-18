b8bd32d66a63427400a444929438d83ba6972cbc
Aakanksha <aakanksha.rathi@icloud.com>
2025-06-18 11:27:42 +0530

    Time: 3 ms (4.56%), Space: 31.4 MB (43.66%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,12 +11,36 @@
  */
 class Solution {
 public:
-    int ans=0;
+    // int ans=0;
+    int rightHeight(TreeNode* root){
+        if(root==NULL)return 0;
+        int height=0;
+        while(root){
+            height++;
+            root=root->right;
+        }
+        return height;
+    }
+    int leftHeight(TreeNode* root){
+        if(root==NULL)return 0;
+        int height=0;
+        while(root){
+            height++;
+            root=root->left;
+        }
+        return height;
+    }
     int countNodes(TreeNode* root) {
         if(root==NULL)return 0;
-        ans+=1;
-        countNodes(root->left);
-        countNodes(root->right);
-        return ans;
+        // ans+=1;
+        // countNodes(root->left);
+        // countNodes(root->right);
+        // return ans;
+        int l=leftHeight(root);
+        int r=rightHeight(root);
+        if(l==r)return (1<<l)-1;
+        else{
+            return 1 + countNodes(root->left)+countNodes(root->right);
+        }
     }
 };
