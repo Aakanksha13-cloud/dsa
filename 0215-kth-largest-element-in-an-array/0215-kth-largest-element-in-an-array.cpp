8a0c10f1ebde35985315134c57f31f79c9697106
Aakanksha <aakanksha.rathi@icloud.com>
2025-06-25 16:23:37 +0530

    Time: 23 ms (81.48%), Space: 61.4 MB (71.13%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -25,6 +25,8 @@ public:
     //     heapify(nums,i,0);
     //    }
     //    return nums[0];
+
+    //ny min heap
         priority_queue<int,vector<int>,greater<int>>pq;
         for(int i=0;i<k;i++){
             pq.push(nums[i]);
@@ -36,6 +38,8 @@ public:
             }
         }
         return pq.top();
+
+        //by max heap
         // priority_queue<int>pq;
         // for(int i=0;i<nums.size();i++){
         //     pq.push(nums[i]);
