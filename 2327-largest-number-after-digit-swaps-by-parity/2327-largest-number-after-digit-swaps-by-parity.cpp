98b879e2331758ad24b5c6cff973b3d5e065d90a
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-04 04:30:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (92.89%) - LeetHub

--- 2327-largest-number-after-digit-swaps-by-parity/2327-largest-number-after-digit-swaps-by-parity.cpp
+++ 2327-largest-number-after-digit-swaps-by-parity/2327-largest-number-after-digit-swaps-by-parity.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int largestInteger(int num) {
-        if(num/10==0)return num;
+        //if(num/10==0)return num;
         vector<int> nums;
         while(num!=0){
             int digit = num%10;
@@ -9,7 +9,7 @@ public:
             num=num/10;
         }
         reverse(nums.begin(),nums.end());
-
+        if(nums.size()==1) return nums[0];
         for(int i =0; i<nums.size()-1;i++){
            for(int j = nums.size()-1; j>i;j--){
             if(nums[i]%2==0){
