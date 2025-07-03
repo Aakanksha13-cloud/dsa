d14a705692a17350e16d2ee0b68a3d259416a21b
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-04 03:14:50 +0530

    Time: 3 ms (83.26%), Space: 15.6 MB (57.4%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<string> ans;
-        // priority_queue<pair<int,int>> pq;
-        // int j=1;
-        // for(int i=0;i<score.size();i++){
-        //     pq.push({score[i],j});
-        //     j++;
-        // }
-        vector<int> copy=score;
-        sort(copy.rbegin(),copy.rend());
-        unordered_map<int,int> m;
-        for(int i=0;i<copy.size();i++){
-            m[copy[i]]=i+1;
-        }
+        int n=score.size();
+        vector<string> ans(n);
+        priority_queue<pair<int,int>> pq;
+        
         for(int i=0;i<score.size();i++){
-            int rank= m[score[i]];
-            if(rank==1)ans.push_back("Gold Medal");
-            else if(rank==2)ans.push_back("Silver Medal");
-            else if(rank==3)ans.push_back("Bronze Medal");
-            else ans.push_back(to_string(rank));
+            pq.push({score[i],i});
         }
-        return ans;
+        int rank=1;
+        while(!pq.empty()){
+            int val=pq.top().first;
+            int index=pq.top().second;
+            pq.pop();
+            if(rank==1)ans[index]="Gold Medal";
+            else if(rank==2)ans[index]="Silver Medal";
+            else if(rank==3)ans[index]="Bronze Medal";
+            else ans[index]=to_string(rank);
+            rank++;
+        }
+        // vector<int> copy=score;
+        // sort(copy.rbegin(),copy.rend());
+        // unordered_map<int,int> m;
+        // for(int i=0;i<copy.size();i++){
+        //     m[copy[i]]=i+1;
+        // }
+        // for(int i=0;i<score.size();i++){
+        //     int rank= m[score[i]];
+        //     if(rank==1)ans.push_back("Gold Medal");
+        //     else if(rank==2)ans.push_back("Silver Medal");
+        //     else if(rank==3)ans.push_back("Bronze Medal");
+        //     else ans.push_back(to_string(rank));
+        // }
+         return ans;
     }
 };
