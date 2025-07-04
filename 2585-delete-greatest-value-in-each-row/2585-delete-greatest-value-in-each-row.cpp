758f8b1f9981dad9f57c14b7a88702ed92490893
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-04 05:38:52 +0530

    Time: 3 ms (42.84%), Space: 16.4 MB (5.04%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.cpp
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
-        // for (auto &row : grid) {
-        // sort(row.rbegin(), row.rend());
-        // }
-        // int ans=0;
-        // for(int i=0;i<grid[0].size();i++){
-        //     int maxrow = INT_MIN;
-        //    for(int j=0;j<grid.size();j++){
-        //       maxrow=max(maxrow,grid[j][i]);
-        //    }
-        //    ans+=maxrow;
-        // }
-        // return ans;
-        int n = grid.size();
-        int m= grid[0].size();
-        vector<priority_queue<int>> heap(n);
-        for(int i=0;i<n;i++){
-            for(int val : grid[i]){
-                heap[i].push(val);
-            }
+        for (auto &row : grid) {
+        sort(row.rbegin(), row.rend());
         }
         int ans=0;
-        for(int j=0;j<m;j++){
-            int maxele= INT_MIN;
-            for(int i=0;i<n;i++){
-                int val = heap[i].top();
-                heap[i].pop();
-                maxele=max(maxele,val);
-            }
-            ans+=maxele;
+        for(int i=0;i<grid[0].size();i++){
+            int maxrow = INT_MIN;
+           for(int j=0;j<grid.size();j++){
+              maxrow=max(maxrow,grid[j][i]);
+           }
+           ans+=maxrow;
         }
         return ans;
+        // int n = grid.size();
+        // int m= grid[0].size();
+        // vector<priority_queue<int>> heap(n);
+        // for(int i=0;i<n;i++){
+        //     for(int val : grid[i]){
+        //         heap[i].push(val);
+        //     }
+        // }
+        // int ans=0;
+        // for(int j=0;j<m;j++){
+        //     int maxele= INT_MIN;
+        //     for(int i=0;i<n;i++){
+        //         int val = heap[i].top();
+        //         heap[i].pop();
+        //         maxele=max(maxele,val);
+        //     }
+        //     ans+=maxele;
+        // }
+        // return ans;
     }
 };
