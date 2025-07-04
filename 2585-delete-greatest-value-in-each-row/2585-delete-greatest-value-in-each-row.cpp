325c281ce4b228a9909e06ab9b2ec9f9b4ca68df
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-04 05:38:27 +0530

    Time: 1 ms (52.92%), Space: 14.5 MB (14.43%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.cpp
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
-        for (auto &row : grid) {
-        sort(row.rbegin(), row.rend());
+        // for (auto &row : grid) {
+        // sort(row.rbegin(), row.rend());
+        // }
+        // int ans=0;
+        // for(int i=0;i<grid[0].size();i++){
+        //     int maxrow = INT_MIN;
+        //    for(int j=0;j<grid.size();j++){
+        //       maxrow=max(maxrow,grid[j][i]);
+        //    }
+        //    ans+=maxrow;
+        // }
+        // return ans;
+        int n = grid.size();
+        int m= grid[0].size();
+        vector<priority_queue<int>> heap(n);
+        for(int i=0;i<n;i++){
+            for(int val : grid[i]){
+                heap[i].push(val);
+            }
         }
         int ans=0;
-        for(int i=0;i<grid[0].size();i++){
-            int maxrow = INT_MIN;
-           for(int j=0;j<grid.size();j++){
-              maxrow=max(maxrow,grid[j][i]);
-           }
-           ans+=maxrow;
+        for(int j=0;j<m;j++){
+            int maxele= INT_MIN;
+            for(int i=0;i<n;i++){
+                int val = heap[i].top();
+                heap[i].pop();
+                maxele=max(maxele,val);
+            }
+            ans+=maxele;
         }
         return ans;
     }
