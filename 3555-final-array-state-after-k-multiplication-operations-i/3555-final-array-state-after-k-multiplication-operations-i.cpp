8d95705cf4689d0379367e3dd184cebb337c0ba9
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-04 06:10:50 +0530

    Time: 1 ms (38.7%), Space: 27.8 MB (9.99%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,35 @@
+class compare{
+    public:
+    bool operator()(const pair<int,int> &a, const pair<int,int> &b){
+        if(a.first==b.first)return a.second > b.second;
+        return a.first>b.first;
+    }
+};
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,compare> pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push({nums[i],i});
+        }
+        int op=0;        
+        while(!pq.empty() && op<k){
+            auto front = pq.top();
+            pq.pop();
+            int val = front.first;
+            int index= front.second;
+            val = val*multiplier;
+            pq.push({val,index});
+            op++;
+        }
+        vector<int> ans(nums.size());
+        while(!pq.empty()){
+            auto front = pq.top();
+            pq.pop();
+            int val = front.first;
+            int index= front.second;
+            ans[index]=val;
+        }
+        return ans;
+    }
+};
