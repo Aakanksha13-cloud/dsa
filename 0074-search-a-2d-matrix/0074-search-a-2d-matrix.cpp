31bf7a03447804839da6624d62cf563a8a5d3e6c
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-09 06:00:49 +0530

    Time: 0 ms (100%), Space: 13.2 MB (75.96%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,9 +6,15 @@ public:
         int i=0;
         while(i<row){
             if(matrix[i][0]<=target && matrix[i][col-1]>=target){
-                for(int j=0;j<col;j++){
-                    if(matrix[i][j]==target)return true;
-                }
+               int s=0;
+               int e=col-1;
+               int mid = (e-s)/2;
+               while(s<=e){
+                if(matrix[i][mid]==target)return true;
+                else if(matrix[i][mid]<target)s=mid+1;
+                else e = mid-1;
+                mid= s+(e-s)/2;
+               }
             }
             i++;
         }
