d69c5168b5d9cd4da932b6fa0eb8a8e7378a5cdc
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-12 23:23:55 +0530

    Time: 0 ms (100%), Space: 112.5 MB (40.15%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        // int extra =0;
-        // int lag =0;
-        // int ans=0;
-        // for(int i=0;i<gas.size();i++){
-        //     if(extra+ gas[i]-cost[i]<0){
-        //         lag+= extra+ gas[i]-cost[i];
-        //         ans=i+1;
-        //         extra=0;
-        //     }
-        //     else{
-        //         extra+= gas[i]-cost[i];
-        //     }
-        // }
-        // if(extra+lag>=0)return ans;
-        // else return -1;
-        
         int start=0;
         int extra=0;
-        int lack=0;
+        int lag=0;
         for(int i=0;i<gas.size();i++){
             if(extra+gas[i]<cost[i]){
-                lack+=extra+gas[i]-cost[i];
+                lag+=extra+ gas[i]-cost[i];
                 extra=0;
-                start=i+1;
+                start = i+1;
             }
-            else{
-                extra+=gas[i]-cost[i];
-            }
+            else extra+= gas[i]-cost[i];
         }
-        if(extra+lack>=0)return start;
-        else return -1;
+        if(extra+lag>=0)return start;
+        return -1;
     }
 };
