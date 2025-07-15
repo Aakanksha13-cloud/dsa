98cd53305c1772938825bf87cfd23a1582f8933f
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-15 12:20:04 +0530

    Time: 19 ms (83.95%), Space: 22 MB (53.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -19,9 +19,29 @@ public:
         dp[amount]=mincoinsUsed;
         return dp[amount];
     }
+    int solveTabulation(vector<int>& coins, int amount){
+        vector<int> dp(amount+1,-1);
+        dp[0]=0;
+        for(int amt=1;amt<=amount;amt++){
+             int mincoinsUsed = INT_MAX;
+            for(int i=0; i<coins.size(); i++){
+            int coin = coins[i];
+            if(coin<=amt){
+                int restCoins =dp[amt-coin];
+                if(restCoins!= INT_MAX){
+                  int coinUsed = 1+ restCoins;
+                  mincoinsUsed = min(mincoinsUsed, coinUsed);
+                }
+            }
+            }
+            dp[amt]=mincoinsUsed;
+        }
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1,-1);
-        int ans = minCoinMem(coins, amount,dp);
+        //int ans = minCoinMem(coins, amount,dp);
+        int ans = solveTabulation(coins, amount);
         if(ans==INT_MAX){
             return -1;
         }
