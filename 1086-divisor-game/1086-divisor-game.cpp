0a3b8a20325b700724f9f72efd68f6e641d8cdba
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-16 07:02:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (6.77%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -3,7 +3,7 @@ public:
     bool rec(int n,vector<int>& dp){
       if(n==1)return false;
       if(dp[n]!=-1)return dp[n];
-      for(int i=1;i<n;i++){
+      for(int i=n-1;i>0;i--){
         if(n%i==0){
             bool recKaAns= rec(n-i,dp);
             if(!recKaAns){
@@ -12,7 +12,7 @@ public:
             }
         }
       }
-      return false;
+      return dp[n]=false;
     }
     bool divisorGame(int n) {
        vector<int> dp(n+1,-1);
