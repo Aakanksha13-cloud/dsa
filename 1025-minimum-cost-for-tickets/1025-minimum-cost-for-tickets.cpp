39be911c310d9cf1a73723439c2f00501bbc24c9
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-17 15:54:18 +0530

    Time: 0 ms (100%), Space: 13.4 MB (49.97%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -8,20 +8,51 @@ public:
         int i7=i;
         for(int index=i+1;index<days.size();index++){
             if(days[index]<day+7)i7++;
+            else break;
         }
         int day7 = costs[1]+rec(days,costs,i7+1,dp);
         int i30=i;
         for(int index=i+1;index<days.size();index++){
             if(days[index]<day+30)i30++;
+            else break;
         }
         int day30= costs[2]+rec(days,costs,i30+1,dp);
         int min1 = min(day1,day7);
         dp[i]= min(day30,min1);
         return dp[i];
     }
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
+    int solveTab(vector<int>& days, vector<int>& costs){
         int n=days.size();
         vector<int> dp(n+1,-1);
-        return rec(days,costs,0,dp);
+        dp[n]=0;
+        
+        for(int i=n-1;i>=0;i--){
+        int day=days[i];
+        int day1=costs[0]+dp[i+1];
+        int i7=i;
+        for(int index=i+1;index<days.size();index++){
+            if(days[index]<day+7)i7++;
+            else break;
+        }
+        int day7 = costs[1]+dp[i7+1];
+        int i30=i;
+        for(int index=i+1;index<days.size();index++){
+            if(days[index]<day+30)i30++;
+            else break;
+        }
+        int day30= costs[2]+dp[i30+1];
+        int min1 = min(day1,day7);
+        dp[i]= min(day30,min1);
+        }
+        return dp[0];
+        
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // int n=days.size();
+        // vector<int> dp(n+1,-1);
+        // return rec(days,costs,0,dp);
+        if(days.size()==1)return min({costs[0],costs[1],costs[2]});
+        return solveTab(days,costs);
     }
 };
