775939b7635bd34c0451a2a33233284fdbfc45b6
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-21 11:01:00 +0530

    Time: 0 ms (100%), Space: 10.4 MB (48.56%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -26,9 +26,31 @@ public:
         return ans;
        
     }
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        
+
+    vector<string> recBU(string& s, vector<string>& wordDict,int i, unordered_map<int,vector<string>>& dp){
+        if(i==s.length()){
+            return {""};
+        }
+        if(dp.find(i)!=dp.end())return dp[i];
+        vector<string> ans;
+        string word="";
+        for(int j=i;j<s.length();j++){
+            word+=s[j];
+            if(check(word,wordDict)){
+                auto recAns= recBU(s,wordDict,j+1,dp);
+                for(auto eachRec:recAns){
+                    string endPart;
+                    if(eachRec.length()>0)endPart=" " + eachRec;
+                    ans.push_back(word+endPart);
+                }
+            }
+        }
+        return dp[i]= ans;
        
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
         return  rec(s,wordDict,0);
+        unordered_map<int,vector<string>> dp;
+        return  recBU(s,wordDict,0,dp);
     }
 };
