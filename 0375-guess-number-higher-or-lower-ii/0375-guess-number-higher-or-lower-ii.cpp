f775bc138ee9934d56c49912c88ee99c932d4126
Aakanksha <aakanksha.rathi@icloud.com>
2025-07-22 15:18:10 +0530

    Time: 31 ms (93.57%), Space: 11.2 MB (22.19%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -9,8 +9,25 @@ public:
         }
         return dp[s][e]=ans;
     }
+    int solveTab(int n){
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int s=n;s>=1;s--){
+            for(int e=s;e<=n;e++){
+               if(s>=e)continue;
+               else{
+                 int ans=INT_MAX;
+        for(int i=s;i<e;i++){
+            ans=min(ans,i+max(dp[i+1][e],dp[s][i-1]));
+        }
+        dp[s][e]=ans;
+               }
+            }
+        }
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return rec(1,n,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return rec(1,n,dp);
+        return solveTab(n);
     }
 };
