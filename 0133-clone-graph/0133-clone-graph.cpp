07b8f82f1d05721cbef7064e22eb44f8f784280f
Aakanksha <aakanksha.rathi@icloud.com>
2025-12-25 04:43:09 +0530

    Time: 3 ms (67.94%), Space: 11.8 MB (96.62%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node==NULL)return NULL;
+        unordered_map<Node*,Node*> m;
+        queue<Node*> q;
+
+        m[node]= new Node(node->val);
+        q.push(node);
+        while(!q.empty()){
+            Node* curr = q.front();
+            q.pop();
+
+            for(auto nbr: curr->neighbors){
+                if(m.find(nbr)==m.end()){
+                    m[nbr]= new Node(nbr->val);
+                    q.push(nbr);
+                }
+                m[curr]->neighbors.push_back(m[nbr]);
+            }
+        }
+        return m[node];
+    }
+};
