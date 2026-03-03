eb6f1cb824a9303fbf948608ee91b2e110d889fe
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-03 17:28:53 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        //onlogn
+        // sort(nums.begin(),nums.end());
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i)return i;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=i)return i;
+        // }
+        // return n;
+
+        //on
+        //cox xor of a,a=0 or cancels out..so only missing one remains
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            ans^=nums[i];
         }
-        return n;
+        for(int i=0;i<=n;i++){
+            ans^=i;
+        }
+        return ans;
     }
 };
