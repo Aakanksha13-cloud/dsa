3285928e8904239181b1e1dbd36831d1cd84cc3b
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-04 22:46:09 +0530

    Time: 7 ms (57.62%), Space: 17.1 MB (55.32%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int s=0;
-        int e=1;
-        //int mid=e/2;
+        // onlogn tc o1 sc
+        // sort(nums.begin(),nums.end());
+        // int s=0;
+        // int e=1;
+        // int ans=0;
+        // while(s<=e && e<nums.size()){
+        //    if(s>0 && nums[s]==nums[s-1]){
+        //     s++;
+        //     continue;
+        //    }
+        //    if((e<nums.size()-1 && nums[e]==nums[e+1]) || s==e){
+        //     e++;
+        //     continue;
+        //    }
+        //    if(abs(nums[e]-nums[s])==k){
+        //     ans++;
+        //     s++;
+        //     e++;
+        //    }
+        //    else if(abs(nums[e]-nums[s])>k)s++;
+        //    else e++;
+        // }
+        // return ans;
+
+        //on tc on sc
         int ans=0;
-        while(s<=e && e<nums.size()){
-           if(s>0 && nums[s]==nums[s-1]){
-            s++;
-            continue;
-           }
-           if((e<nums.size()-1 && nums[e]==nums[e+1]) || s==e){
-            e++;
-            continue;
-           }
-           if(abs(nums[e]-nums[s])==k){
-            ans++;
-            s++;
-            e++;
-           }
-           else if(abs(nums[e]-nums[s])>k)s++;
-           else e++;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto it:m){
+            if(k==0){
+                if(it.second>1)ans++;
+            }
+            else if(m.find(it.first + k)!=m.end())ans++;
         }
         return ans;
     }
