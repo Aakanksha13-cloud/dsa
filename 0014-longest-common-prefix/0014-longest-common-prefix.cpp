95a74d12c5bb8e8f01cf407b0a94b2f04c1bde51
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-06 16:22:19 +0530

    Time: 4 ms (6.71%), Space: 11.7 MB (82.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,75 +1,87 @@
-class TrieNode{
-    public:
-    char val;
-    unordered_map<char,TrieNode*> children;
-    bool isTerminal;
-    TrieNode(char data){
-        val=data;
-        isTerminal = false;
-    }
-};
-void insert(TrieNode* root, string word){
-    if(word.length()==0){
-        root->isTerminal=true;
-        return;
-    }
-    char ch=word[0];
-    TrieNode* child;
-    if(root->children.find(ch)!=root->children.end()){
-        child=root->children[ch];
-    }
-    else{
-        child = new TrieNode(ch);
-        root->children[ch]=child;
-    }
-    insert(child,word.substr(1));
-}
+// class TrieNode{
+//     public:
+//     char val;
+//     unordered_map<char,TrieNode*> children;
+//     bool isTerminal;
+//     TrieNode(char data){
+//         val=data;
+//         isTerminal = false;
+//     }
+// };
+// void insert(TrieNode* root, string word){
+//     if(word.length()==0){
+//         root->isTerminal=true;
+//         return;
+//     }
+//     char ch=word[0];
+//     TrieNode* child;
+//     if(root->children.find(ch)!=root->children.end()){
+//         child=root->children[ch];
+//     }
+//     else{
+//         child = new TrieNode(ch);
+//         root->children[ch]=child;
+//     }
+//     insert(child,word.substr(1));
+// }
 class Solution {
 public:
-    void solve(TrieNode* root, string& ans){
-        if(root->isTerminal == true)return;
-        TrieNode* child;
-        if(root->children.size()==1){
-            for(auto i:root->children){
-                char ch = i.first;
-                ans.push_back(ch);
-                child = i.second;
-            }
-        }
-        else return;
-        solve(child,ans);
-    }
+//     void solve(TrieNode* root, string& ans){
+//         if(root->isTerminal == true)return;
+//         TrieNode* child;
+//         if(root->children.size()==1){
+//             for(auto i:root->children){
+//                 char ch = i.first;
+//                 ans.push_back(ch);
+//                 child = i.second;
+//             }
+//         }
+//         else return;
+//         solve(child,ans);
+//     }
     string longestCommonPrefix(vector<string>& strs) {
-        TrieNode* root = new TrieNode('-');
-        for(auto str:strs){
-            insert(root,str);
-        }
-        string ans="";
-        solve(root,ans);
-        return ans;
-        // string ans;
-        // int i = 0;
-        // while(true){
-        //     char curr_ch = 0;
-        //     for(auto str:strs){
-        //         if(i>= str.size()){
-        //             curr_ch =0;
-        //             break;
-        //         }
-        //         if(curr_ch == 0){
-        //             curr_ch = str[i];
-        //         }
-        //         if(curr_ch != str[i]){
-        //              curr_ch =0;
-        //             break;
-        //         }
-        //     }
-        //     if(curr_ch == 0)
-        //       break;
-
-        //     ans.push_back(curr_ch);
-        //     i++;
+        // TrieNode* root = new TrieNode('-');
+        // for(auto str:strs){
+        //     insert(root,str);
         // }
+        // string ans="";
+        // solve(root,ans);
         // return ans;
+        // // string ans;
+        // // int i = 0;
+        // // while(true){
+        // //     char curr_ch = 0;
+        // //     for(auto str:strs){
+        // //         if(i>= str.size()){
+        // //             curr_ch =0;
+        // //             break;
+        // //         }
+        // //         if(curr_ch == 0){
+        // //             curr_ch = str[i];
+        // //         }
+        // //         if(curr_ch != str[i]){
+        // //              curr_ch =0;
+        // //             break;
+        // //         }
+        // //     }
+        // //     if(curr_ch == 0)
+        // //       break;
+
+        // //     ans.push_back(curr_ch);
+        // //     i++;
+        // // }
+        // // return ans;
+        int n=strs.size();
+        string ans="";
+        int i=0;
+        while(i<strs[0].length()){
+            char c=strs[0][i];
+            for(int j=1;j<n;j++){
+                if(i>=strs[j].length() ||  strs[j][i]!=c)return ans;
+            }
+            ans.push_back(c);
+            i++;
+        }
+        return ans;
     }
 };
