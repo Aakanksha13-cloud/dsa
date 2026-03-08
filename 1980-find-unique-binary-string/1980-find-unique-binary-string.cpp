71b9a505c1948ab28a2b3a44a4061be75302e937
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-08 15:19:25 +0530

    Time: 0 ms (100%), Space: 13.2 MB (26.18%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+string ans="";
+    void backtrack(int i,int n,string &curr,unordered_set<string>& st){
+        if(i==n){
+            if(st.find(curr)==st.end()){
+                ans=curr;
+            }
+             return;
+        }
+        if(ans!="")return;
+        curr+='0';
+        backtrack(i+1,n,curr,st);
+        curr.pop_back();
+
+        curr+='1';
+        backtrack(i+1,n,curr,st);
+        curr.pop_back();
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n= nums.size();
+        string curr="";
+        unordered_set<string> st(nums.begin(),nums.end());
+        backtrack(0,n,curr,st);
+        return ans;
+    }
+};
