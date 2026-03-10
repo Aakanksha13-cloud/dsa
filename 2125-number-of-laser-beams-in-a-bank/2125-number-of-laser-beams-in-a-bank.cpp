9db6c87c00f3e74e3f5c12caab6ee9cc97551e44
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-10 22:50:00 +0530

    Time: 6 ms (58.28%), Space: 33.4 MB (5.54%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int n=bank.size();
-        int r1=0;
-        int r2=1;
-        int ans=0;
-        
-        while(r2>r1 && r2<n && r1<n-1){
-            int a=0;
-            for(int i=r1;i<n;i++){    
-                    string s=bank[i];
-                    for(auto st:s)if(st=='1')a++;
-                    if(a>0){
-                        r1=i;
-                        break;
-                    }    
-            }
-            int b=0;  
-            for(int i=r1+1;i<n;i++){  
-                    string s=bank[i];
-                    for(auto st:s)if(st=='1')b++;
-                    if(b>0){
-                        r2=i;
-                        break;
-                }    
-            }
+        //int n=bank.size();
+        //on2m
+        // int r1=0;
+        // int r2=1;
+        // int ans=0;
+        // while(r2>r1 && r2<n && r1<n-1){
+        //     int a=0;
+        //     for(int i=r1;i<n;i++){    
+        //             string s=bank[i];
+        //             for(auto st:s)if(st=='1')a++;
+        //             if(a>0){
+        //                 r1=i;
+        //                 break;
+        //             }    
+        //     }
+        //     int b=0;  
+        //     for(int i=r1+1;i<n;i++){  
+        //             string s=bank[i];
+        //             for(auto st:s)if(st=='1')b++;
+        //             if(b>0){
+        //                 r2=i;
+        //                 break;
+        //         }    
+        //     }
 
-            int laser = a*b;
-            ans+=laser;
-            r1=r2;
-            r2=r1+1;
+        //     int laser = a*b;
+        //     ans+=laser;
+        //     r1=r2;
+        //     r2=r1+1;
            
+        // }
+        // return ans;
+        vector<int> one;
+        for(string b:bank){
+            int c=0;
+            for(auto s:b)if(s=='1')c++;
+            if(c>0)one.push_back(c);
+        }
+        if(one.size()==1)return 0;
+        int ans=0;
+        for(int i=1;i<one.size();i++){
+           int pro=one[i]*one[i-1];
+           ans+=pro;
         }
         return ans;
     }
