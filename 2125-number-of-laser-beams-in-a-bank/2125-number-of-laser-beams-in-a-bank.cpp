d90c8d3a592c136e16732e2f0b79d85a1b36760a
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-10 22:45:09 +0530

    Time: 11 ms (30.44%), Space: 38.2 MB (5.54%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -5,6 +5,7 @@ public:
         int r1=0;
         int r2=1;
         int ans=0;
+        
         while(r2>r1 && r2<n && r1<n-1){
             int a=0;
             for(int i=r1;i<n;i++){    
@@ -15,19 +16,21 @@ public:
                         break;
                     }    
             }
-            int b=0;
-            for(int i=r1+1;i<n;i++){    
+            int b=0;  
+            for(int i=r1+1;i<n;i++){  
                     string s=bank[i];
                     for(auto st:s)if(st=='1')b++;
                     if(b>0){
                         r2=i;
                         break;
-                    }    
+                }    
             }
+
             int laser = a*b;
             ans+=laser;
             r1=r2;
             r2=r1+1;
+           
         }
         return ans;
     }
