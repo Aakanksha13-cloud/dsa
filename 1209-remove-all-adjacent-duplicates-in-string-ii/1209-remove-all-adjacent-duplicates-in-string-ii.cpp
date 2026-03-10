9b84a8057eda5485f64c517cbb7a5ed842ce1085
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-11 00:04:58 +0530

    Time: 3 ms (85.4%), Space: 13.6 MB (91.57%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    bool lastkChar(string& ans,char ch, int x){
-        int len=(int)ans.length();
-        for(int j=len-1;j>=len-x;j--){
-            if(ans[j]!=ch)return false;
-        }
-        return true;
-    }
-    string removeDuplicates(string s, int k) {
-        string ans="";
-        for(int i=0;i<s.length();i++){
-            char ch=s[i];
-            if(ans.length()<k-1)ans.push_back(ch);
-            else{
-                if(lastkChar(ans,ch,k-1)){
-                    for(int j=0;j<k-1;j++)ans.pop_back();
-                }
-                else ans.push_back(ch);
-            }
-        }
-        return ans;
+    //bool lastkChar(string& ans,char ch, int x){
+    //     int len=(int)ans.length();
+    //     for(int j=len-1;j>=len-x;j--){
+    //         if(ans[j]!=ch)return false;
+    //     }
+    //     return true;
+    // }
+     string removeDuplicates(string s, int k) {
+    //     string ans="";
+    //     for(int i=0;i<s.length();i++){
+    //         char ch=s[i];
+    //         if(ans.length()<k-1)ans.push_back(ch);
+    //         else{
+    //             if(lastkChar(ans,ch,k-1)){
+    //                 for(int j=0;j<k-1;j++)ans.pop_back();
+    //             }
+    //             else ans.push_back(ch);
+    //         }
+    //     }
+    //     return ans;
+       vector<pair<char,int>> count;
+       for(int i=0;i<s.length();i++){
+        char ch=s[i];
+        if(count.size()!=0 && count.back().first==ch)count.back().second++;
+        else count.push_back({ch,1});
+        if(count.back().second>=k)count.pop_back();
+       }
+       string ans="";
+       for(auto c:count){
+         int freq=c.second;
+         while(freq>0){
+            ans.push_back(c.first);
+            freq--;
+         }
+       }
+       return ans;
     }
 };
