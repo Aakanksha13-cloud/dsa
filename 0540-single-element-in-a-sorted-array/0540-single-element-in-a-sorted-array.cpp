8a4b6b61e1e045cd74cd34cab63215964dfbb907
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-13 17:09:18 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.57%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,18 +4,51 @@ public:
         //approach: if mid: even-> single ele at left or mid 
         //mid:odd->single ele at right
 
+        // int s=0;
+        // int e=nums.size()-1;
+        // int mid=e/2;
+        // while(s<e){
+        //     if(mid%2==1)mid--;
+        //     if(nums[mid]==nums[mid+1])s=mid+2;
+        
+        //     else{
+        //         e=mid;
+        //     }
+        //     mid=s+(e-s)/2;
+        // }
+        // return nums[s];
+
+        // int s=0;
+        // int n=arr.size();
+        // int e=arr.size()-1;
+        // int mid=e/2;
+        // while(s<=e){
+        //     if(s==e)return mid;
+        //     if(mid-1>=0 && arr[mid]!=arr[mid-1] && mid+1<n && arr[mid]!=arr[mid+1])arr[mid];
+        //     if(mid-1>=0 && arr[mid]==arr[mid-1]){
+        //         int startindex=mid-1;
+        //         if(startindex%2!=0)e=mid-1;
+        //         else s=mid+1;
+        //     }
+        //     else if(mid+1<n && arr[mid]==arr[mid+1]){
+        //         int startindex=mid;
+        //         if(startindex%2!=0)e=mid-1;
+        //         else s=mid+1;
+        //     }
+        //     mid=s+(e-s)/2;
+        // }
+
         int s=0;
-        int e=nums.size()-1;
+        int n=nums.size();
+        int e=n-1;
         int mid=e/2;
         while(s<e){
             if(mid%2==1)mid--;
             if(nums[mid]==nums[mid+1])s=mid+2;
-        
-            else{
-                e=mid;
-            }
+            else e=mid;
             mid=s+(e-s)/2;
         }
         return nums[s];
+        return -1;
     }
 };
