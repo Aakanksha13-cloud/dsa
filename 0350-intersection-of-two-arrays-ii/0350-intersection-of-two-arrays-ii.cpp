94fe1a3661841bedfcf07dcdf9c95a27f474fe5a
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-13 22:28:57 +0530

    Time: 1 ms (40.86%), Space: 14.9 MB (15.85%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        //onlogn + mlogm
+        // int n1=nums1.size();
+        // int n2=nums2.size();
+        // sort(nums1.begin(),nums1.end());
+        // sort(nums2.begin(),nums2.end());
+        // vector<int> ans;
+        // int i=0;
+        // int j=0;
+        // while(i<n1 && j<n2){
+        //     if(nums1[i]==nums2[j]){
+        //         ans.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums1[i]<nums2[j])i++;
+        //     else j++;
+        // }
+        // return ans;
+
+        //O(n)
+        if(nums1.size()>nums2.size())return intersect(nums2,nums1);
         int n1=nums1.size();
         int n2=nums2.size();
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
+        unordered_map<int,int> m;
+        for(int n:nums1){
+            m[n]++;
+        }
         vector<int> ans;
-        int i=0;
-        int j=0;
-        while(i<n1 && j<n2){
-            if(nums1[i]==nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
+        for(int x:nums2){
+            if(m[x]>0){
+                ans.push_back(x);
+                m[x]--;
             }
-            else if(nums1[i]<nums2[j])i++;
-            else j++;
         }
         return ans;
     }
