4ad240a2b3d05988f1d508c49a26d1231d83e973
Aakanksha <aakanksha.rathi@icloud.com>
2026-03-27 00:56:18 +0530

    Time: 2 ms (39.82%), Space: 10.3 MB (83.8%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-void solve(vector<int>& nums,set<vector<int>>& ans,vector<int>& curr, int i){
-        if(i==nums.size())return;
-        int num=nums[i];
-        //include
+// void solve(vector<int>& nums,set<vector<int>>& ans,vector<int>& curr, int i){
+//         if(i==nums.size())return;
+//         int num=nums[i];
+//         //include
+//         curr.push_back(nums[i]);
+//         ans.insert(curr);
+//         solve(nums,ans,curr,i+1);
+//         //exclude
+//         curr.pop_back();
+//         solve(nums,ans,curr,i+1);
+//     }
+void solve(vector<int>& nums, vector<vector<int>>& result, vector<int>& curr, int index) {
+    result.push_back(curr);
+
+    for(int i = index; i < nums.size(); i++){
+        if(i > index && nums[i] == nums[i-1]) continue;
+
         curr.push_back(nums[i]);
-        ans.insert(curr);
-        solve(nums,ans,curr,i+1);
-        //exclude
+        solve(nums, result, curr, i + 1);
         curr.pop_back();
-        solve(nums,ans,curr,i+1);
     }
+}
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> result;
         sort(nums.begin(),nums.end());
-        set<vector<int>> ans;
+        //set<vector<int>> ans;
         vector<int> curr={};
-        ans.insert(curr);
-        solve(nums,ans,curr,0);
-        for(auto st:ans){
-            result.push_back(st);
-        }
+        //ans.insert(curr);
+        solve(nums,result,curr,0);
+        // for(auto st:ans){
+        //     result.push_back(st);
+        // }
         
         return result;
     }
