a0f200bd9931ee7ffd85b6f65d47e8b07782fcac
Aakanksha <aakanksha.rathi@icloud.com>
2026-04-04 14:18:34 +0530

    Time: 4 ms (27.86%), Space: 14.2 MB (45.44%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        int curr=1;
+        for(int i=1;i<=n;i++){
+            ans.push_back(curr);
+           //find next no
+            if(curr*10<=n){   
+                curr*=10;
+            }
+            else{
+                while(curr%10==9 || curr+1>n){
+                    curr/=10;
+                }
+                curr++;
+            }
+        }
+        
+        return ans;
+    }
+};
