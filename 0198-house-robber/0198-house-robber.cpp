cfe099386e2bcd7c289323b3d2e469fc69bbf0d8
Aakanksha <aakanksha.rathi@icloud.com>
2026-05-29 07:58:23 +0530

    Time: 0 ms (100%), Space: 10.7 MB (42.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    // int maxMoneyTD(vector<int>& nums, int index,vector<int>& dp){
-    //    if(index >= nums.size()){
-    //       return 0;
-    //    }
-    //    if(dp[index]!=-1)return dp[index];
-       
-    //    int include = nums[index]+ maxMoneyTD(nums,index+2,dp);
-    //    int exclude = 0+ maxMoneyTD(nums,index+1,dp);
-    //    dp[index] = max(include, exclude);
-    //    return dp[index];
-    // }
+    int solve(vector<int>& nums,vector<int>& dp,int i){
+        if(i>=nums.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        int opt1=nums[i]+solve(nums,dp,i+2);
+        int opt2=solve(nums,dp,i+1);
+        dp[i]=max(opt1,opt2);
+        return dp[i];
+    }
     int rob(vector<int>& nums) {
-        // int i =0;
-        // int n=nums.size();
-        // vector<int> dp(n+1,-1);
-        // return maxMoneyTD(nums,i,dp);
-        int n = nums.size();
-        int next1=0;
-        int next2=0;
-        for(int i=n-1;i>=0;i--){
-           int include = nums[i]+next2;
-           int exclude = next1;
-           int curr=max(include,exclude);
-           next2=next1;
-           next1=curr;
-        }
-        return next1;
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(nums,dp,0);
     }
 };
