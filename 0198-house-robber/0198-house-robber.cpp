5af3e8fab36b0dd83ec496de306d56c9cf965f05
Aakanksha <aakanksha.rathi@icloud.com>
2026-05-29 08:06:27 +0530

    Time: 0 ms (100%), Space: 10 MB (88.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,vector<int>& dp,int i){
-        if(i>=nums.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-        int opt1=nums[i]+solve(nums,dp,i+2);
-        int opt2=solve(nums,dp,i+1);
-        dp[i]=max(opt1,opt2);
-        return dp[i];
-    }
+    // int solve(vector<int>& nums,vector<int>& dp,int i){
+    //     if(i>=nums.size())return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     int opt1=nums[i]+solve(nums,dp,i+2);
+    //     int opt2=solve(nums,dp,i+1);
+    //     dp[i]=max(opt1,opt2);
+    //     return dp[i];
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        return solve(nums,dp,0);
+        // vector<int> dp(n+1,-1);
+        // return solve(nums,dp,0);
+        int next=0;
+        int altern=0;
+        for(int i=n-1;i>=0;i--){
+            int inc=nums[i]+altern;
+            int exc=next;
+            int curr=max(inc,exc);
+            altern=next;
+            next=curr;  
+        }
+        return next;
     }
 };
