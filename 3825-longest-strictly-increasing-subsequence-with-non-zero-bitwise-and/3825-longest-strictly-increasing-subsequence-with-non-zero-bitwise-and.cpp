0c498574451fdac5ba1fba08888091f5495cec03
Aakanksha <aakanksha.rathi@icloud.com>
2026-06-23 21:34:46 +0530

    Time: 452 ms (65.57%), Space: 179.5 MB (68.85%) - LeetHub

--- 3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.cpp
+++ 3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and/3825-longest-strictly-increasing-subsequence-with-non-zero-bitwise-and.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
         int ans = 0;
-
         for (int bit = 0; bit < 31; bit++) {
             vector<int> lis;
-
             for (int x : nums) {
                 if ((x >> bit) & 1) {
                     auto it = lower_bound(lis.begin(), lis.end(), x);
-
                     if (it == lis.end())
                         lis.push_back(x);
                     else
                         *it = x;
                 }
             }
-
             ans = max(ans, (int)lis.size());
         }
-
         return ans;
     }
 };
