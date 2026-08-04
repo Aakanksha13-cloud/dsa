9518a1c2095ce9f5eaffc1f1068b9cf7bd627a53
Aakanksha <aakanksha.rathi@icloud.com>
2026-08-05 05:11:41 +0530

    Time: 17 ms (7.23%), Space: 21.6 MB (5.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=prerequisites.size();
         unordered_map<int,vector<int>> adj;
-        for(int i=0;i<n;i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for(auto p:prerequisites){
+            adj[p[1]].push_back(p[0]);
         }
         vector<int> indegree(numCourses,0);
         for(auto a:adj){
-            for(int nbr:a.second){
-               indegree[nbr]++; 
-            }    
+            for(int nbr:a.second)indegree[nbr]++;
         }
         queue<int> q;
+        for(int i=0;i<numCourses;i++){
+           if(indegree[i]==0)q.push(i);
+        }
         vector<int> topo;
-        for(int i=0;i<numCourses;i++)if(indegree[i]==0)q.push(i);
         while(!q.empty()){
             int t=q.front();
             topo.push_back(t);
