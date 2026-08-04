4d94040c1a9dad7b9de25b9fa58f84bbe18856e3
Aakanksha <aakanksha.rathi@icloud.com>
2026-08-04 18:11:11 +0530

    Time: 1 ms (36.62%), Space: 17.2 MB (28.66%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,41 +3,36 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int drn[4][2]={{-1,0},{1,0},{0,-1},{0,1}};
+        int minutes=0;
+        int fresh=0;
         queue<pair<int,int>> q;
-        int minutes =0;
-        int fresh =0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }
-                else if(grid[i][j]==1){
-                    fresh++;
+                if(grid[i][j]==2)q.push({i,j});
+                else if(grid[i][j]==1)fresh++;
+            }
+        }
+        vector<vector<int>> drn={{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.empty() && fresh>0){
+            int s=q.size();
+            minutes++;
+            for(int i=0;i<s;i++){
+                auto t=q.front();
+                int r=t.first;
+                int c=t.second;
+                q.pop();
+                for(int j=0;j<4;j++){
+                    int nr=r+drn[j][0];
+                    int nc=c+drn[j][1];
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
+                        grid[nr][nc]=2;
+                        fresh--;
+                        q.push({nr,nc});
+                    }
                 }
             }
         }
-        while(!q.empty() && fresh>0){
-                       int simultaneousMin = q.size();
-                       minutes++;
-                       for(int i=0;i<simultaneousMin;i++){
-                          auto frontNode= q.front();
-                          q.pop();
-                          int r= frontNode.first;
-                          int c= frontNode.second;
-                          for(auto d:drn){
-                          int nr = r+ d[0];
-                          int nc = c+d[1];
-                          if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-                            grid[nr][nc]=2;
-                            q.push({nr,nc});
-                            fresh--;
-                          }
-                       }
-                       }
-                       
-                    }
-        if(fresh>0) return -1;
+        if(fresh>0)return -1;
         return minutes;
     }
 };
