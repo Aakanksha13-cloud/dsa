f55bf6aac480e3deaa0e8dee9ff46cc46428d9b6
Aakanksha <aakanksha.rathi@icloud.com>
2026-08-06 14:30:00 +0530

    Time: 41 ms (9.44%), Space: 27.6 MB (6.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
     void bfs(vector<vector<char>>& grid,int i,int j){
-       if(i<0 || i>=grid.size()|| j<0 || j>= grid[0].size())return; 
-       
-       queue<pair<int,int>> q;
-       q.push({i,j});
-       grid[i][j]='0';
-       while(!q.empty()){
-        auto frontNode= q.front();
-        i=frontNode.first;
-        j=frontNode.second;
-        q.pop();
-        int up = i-1;
-        if(up>= 0 && up< grid.size() && grid[up][j]=='1'){
-            q.push({up,j});
-            grid[up][j]= '0';
+        int m=grid.size();
+        int n=grid[0].size();
+        if(i<0 || j<0 || i>=m || j>=n)return;
+        grid[i][j]='0'; //visited
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        vector<vector<int>> drn={{-1,0},{1,0},{0,1},{0,-1}};
+        while(!q.empty()){
+            auto top=q.front();
+            int x=top.first;
+            int y=top.second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nx=x+drn[i][0];
+                int ny=y+drn[i][1];
+                if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]=='1'){
+                    q.push({nx,ny});
+                    grid[nx][ny]='0';
+                }
+            }
         }
-        int down = i+1;
-        if(down>= 0 && down< grid.size() && grid[down][j]=='1'){
-            q.push({down,j});
-            grid[down][j]='0';
-        }
-        int left = j-1;
-        if(left>=0 && left<grid[0].size() && grid[i][left]=='1'){
-            q.push({i,left});
-            grid[i][left] ='0';
-        }
-        int right = j+1;
-        if(right>=0 && right<grid[0].size() && grid[i][right]=='1'){
-            q.push({i,right});
-            grid[i][right]='0';
-        }
-       }
-       return;
+        return;
     }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int ans= 0;
+        int m=grid.size();
+        int n=grid[0].size();
+        int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                   bfs(grid,i,j);
-                   ans++; 
+                    bfs(grid,i,j);
+                    ans++;
                 }
             }
         }
